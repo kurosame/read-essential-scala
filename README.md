ef5d3f623b2ccf7a565b56614de1b91b283bfd5c
kurosame <kurosame1227@gmail.com>
2021-07-04 01:24:34 +0900

    add: 1.1 ~ 1.2

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # read-essential-scala
+https://books.underscore.io/essential-scala/essential-scala.html
---
+++ src/main/scala/sandbox/Chapter1.scala
@@ -0,0 +1,22 @@
+package sandbox
+
+object Chapter1 {
+  // 1 Getting Started
+
+  // 1.1 Setting up the Scala Console
+
+  // 1.1.1 Entering Single-Line Expressions
+
+  // 1.1.2 Entering Multi-Line Expressions
+
+  // 1.1.3 Printing the Type of an Expression
+
+  // 1.2 Setting up Scala IDE
+
+  // 1.2.1 Creating your First Application
+//  def main(args: Array[String]): Unit = {
+//    println("Hello world!")
+//  }
+
+  // 1.2.2 Creating your First Worksheet
+}
